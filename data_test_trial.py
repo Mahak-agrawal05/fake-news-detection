b1c79d6d1e23311b2a13268720f790e2f2940a49
Mahak Agrawal <agrawalmahak179@gmail.com>
2026-05-21 15:24:06 +0530

    Remove unnecessary file

--- data_test_trial.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-Data = {
-    "Name" : ["Mahak", "Rahuk", "Aman"],
-    "Marks" : [90, 82, 95]
-}
-df = pd.DataFrame(Data)
-print(df)
-print(df["Name"])
-print(df["Marks"].mean())
